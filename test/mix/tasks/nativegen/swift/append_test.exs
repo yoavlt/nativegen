6d02dc4dd29dfb162b2da24e01a73a189bf68101
Takuma Yoshida <yoa.jmpr.w@gmail.com>
2015-08-13 16:55:57 +0900

    Delete append command

--- test/mix/tasks/nativegen/swift/append_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule Nativegen.Swift.AppendTest do
-  use ExUnit.Case
-
-  import Mix.Tasks.Nativegen.Swift.Append
-
-  @valid_attr ["test_generate_directory/test/UserRepository.swift", "responseMessage", "Post", "/api/chat/response", "thread_id:integer", "message:string"]
-
-end
