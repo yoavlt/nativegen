8068eca1838f081fa3fc7967f8a6e3859d8792a3
Takuma Yoshida <yoa.jmpr.w@gmail.com>
2015-08-14 15:01:29 +0900

    Add coverage

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Nativegen.Mixfile do
      elixir: "~> 1.0",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps]
+     deps: deps,
+     description: description,
+     package: package,
+     test_coverage: [tool: Coverex.Task, coveralls: true]
+   ]
   end
 
   # Configuration for the OTP application
@@ -15,6 +19,19 @@ defmodule Nativegen.Mixfile do
   # Type `mix help compile.app` for more information
   def application do
     [applications: [:logger]]
+  end
+
+  defp description do
+    """
+    Accessible REST API code generator for native app.
+    """
+  end
+
+  defp package do
+    [contributors: ["Takuma Yoshida"],
+      licenses: ["MIT License"],
+      links: %{"GitHub" => "https://github.com/yoavlt/nativegen"},
+      files: ~w(mix.exs README.md lib)]
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +44,8 @@ defmodule Nativegen.Mixfile do
   #
   # Type `mix help deps` for more examples and options
   defp deps do
-    []
+    [
+      {:coverex, "~> 1.4.1", only: :test}
+    ]
   end
 end
